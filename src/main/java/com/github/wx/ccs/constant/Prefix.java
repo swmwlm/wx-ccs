f085d73911e5584e9fe021896d00d1a7d5faf92e
sunwm <sunwm@test.com>
2022-09-29 10:57:21 +0800

    微信小程序中控服务器

--- src/main/java/com/github/wx/ccs/constant/Prefix.java
+++ src/main/java/com/github/wx/ccs/constant/Prefix.java
@@ -2,22 +2,22 @@ package com.github.wx.ccs.constant;
 
 public interface Prefix {
 
-    String MINI_SESSION_KEY = "CSS_MINI_SESSION_KEY_";
+    String MINI_SESSION_KEY = "CCS_MINI_SESSION_KEY_";
 
-    String MINI_OPEN_ID = "CSS_MINI_OPEN_ID_";
+    String MINI_OPEN_ID = "CCS_MINI_OPEN_ID_";
 
-    String MINI_UNION_ID = "CSS_MINI_UNION_ID";
+    String MINI_UNION_ID = "CCS_MINI_UNION_ID";
 
-    String MINI_ACCESS_TOKEN = "CSS_MINI_ACCESS_TOKEN_";
+    String MINI_ACCESS_TOKEN = "CCS_MINI_ACCESS_TOKEN_";
 
-    String ACCESS_TOKEN = "CSS_ACCESS_TOKEN_";
+    String ACCESS_TOKEN = "CCS_ACCESS_TOKEN_";
 
-    String REFRESH_TOKEN = "CSS_REFRESH_TOKEN_";
+    String REFRESH_TOKEN = "CCS_REFRESH_TOKEN_";
 
-    String OPEN_ID = "CSS_OPEN_ID_";
+    String OPEN_ID = "CCS_OPEN_ID_";
 
-    String UNION_ID = "CSS_OPEN_ID_";
+    String UNION_ID = "CCS_OPEN_ID_";
 
-    String USER_INFO = "CSS_USER_INFO_";
+    String USER_INFO = "CCS_USER_INFO_";
 
 }
